e2095f0389f0f638d2e762a415955facdeaac6e8
Prini <nprinitha5@gmail.com>
2021-03-19 21:12:08 -0700

    Adding another test comment

--- src/test/scala/HelloWorldSuite.scala
+++ src/test/scala/HelloWorldSuite.scala
@@ -4,6 +4,7 @@ object HelloWorldSuite extends HelloWorld {
     //  val obj = HelloWorld
     def main(args: Array[String]): Unit = {
       // Typed Hello World
+      //Adding another test comment
       println("Hello World")
       println(s"The value is ${a}")
     }
